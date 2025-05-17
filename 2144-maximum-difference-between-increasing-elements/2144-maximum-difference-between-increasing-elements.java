0ce12db89267957c059e3c70e7ffeaa9283332bd
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2025-05-17 21:46:38 +0530

    Time: 6 ms (7.45%), Space: 42.6 MB (6.89%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
@@ -10,6 +10,7 @@ class Solution {
                 else{
                     continue;
                 }
+                
                 maxdiff=Math.max(diff , maxdiff);
             }
         }
