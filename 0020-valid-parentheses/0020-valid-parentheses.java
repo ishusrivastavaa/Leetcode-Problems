b9b7c6977280c4a8a240d6e61d634b5e644980e7
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2025-07-14 14:31:39 +0530

    Time: 2 ms (96.73%), Space: 41.6 MB (92.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,49 +1,25 @@
-// class Solution {
-//     public boolean isValid(String s) {
-//        Stack<Character> s1= new Stack<>();
-//        for(int i=0 ; i<s.length(); i++){
-//         char ch=s.charAt(i);
-//         if(ch=='(' || ch=='{' || ch=='['){
-//             s1.push(ch);
-//         }
-//         else if(ch==')' || ch=='}' || ch==']'){
-//             if(s1.peek()=='(' && ch== ')' ||
-//               s1.peek()=='{' && ch== '}' ||s1.peek()=='[' && ch== ']'  ){
-//                 s1.pop();
-//             }
-//             else{
-//                 return false;
-//             }
-//         }
-//        }
-//        if(s.isEmpty()){
-//             return true;
-//        }
-//        else{
-//         return false;
-//        }       
-//     }
-// }
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> s1 = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(' || ch == '{' || ch == '[') {
-                s1.push(ch);
-            } else if (ch == ')' || ch == '}' || ch == ']') {
-                if (s1.isEmpty()) return false; // prevent peek() on empty stack
-                char top = s1.peek();
-                if ((top == '(' && ch == ')') ||
-                    (top == '{' && ch == '}') ||
-                    (top == '[' && ch == ']')) {
-                    s1.pop();
-                } else {
-                    return false;
-                }
+       Stack<Character> s1= new Stack<>();
+       for(int i=0 ; i<s.length(); i++){
+        char ch=s.charAt(i);
+        if(ch=='(' || ch=='{' || ch=='['){
+            s1.push(ch);
+        }
+        else if(ch==')' || ch=='}' || ch==']'){
+            if(s1.isEmpty()){
+                return false;
+            }
+            if(s1.peek()=='(' && ch== ')' ||
+              s1.peek()=='{' && ch== '}' ||s1.peek()=='[' && ch== ']'  ){
+                s1.pop();
+            }
+            else{
+                return false;
             }
         }
-        return s1.isEmpty(); // stack should be empty if all matched
+       }
+       return s1.isEmpty(); 
     }
 }
+
