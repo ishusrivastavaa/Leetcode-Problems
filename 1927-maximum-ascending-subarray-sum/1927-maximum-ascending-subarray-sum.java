7237ab6ad25db6826c95a0422356ab248d30fb61
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2025-08-26 01:05:45 +0530

    Time: 0 ms (100%), Space: 41.2 MB (66.41%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -1,46 +1,21 @@
-// class Solution {
-//     public int maxAscendingSum(int[] nums) {
-//         int max=Integer.MIN_VALUE;
-//         int sum=nums[0];
-//         if(nums.length==1){
-//             return nums[0];
-//         }
-//         for(int i=1 ; i<nums.length ; i++){
-//             if(nums[i]>=nums[i-1]){
-//                 sum+=nums[i];
-//             }
-            
-           
-//             else {
-//                 max=Math.max(sum,max);
-//                 sum=nums[i];
-            
-//             }
-
-
-//         }
-//         max=Math.max(max,sum);
-//         return max;
-//     }
-// }
-
 class Solution {
     public int maxAscendingSum(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int sum = nums[0]; // Start with the first element
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                sum += nums[i]; // Continue ascending
-            } else {
-                max = Math.max(max, sum); // Update max before resetting
-                sum = nums[i]; // Start new subarray
+        int max=Integer.MIN_VALUE;
+        int sum=nums[0];
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i=1 ; i<nums.length ; i++){
+            if(nums[i]>nums[i-1]){
+                sum+=nums[i];
+            }
+            else {
+                max=Math.max(sum,max);
+                sum=nums[i];
             }
         }
-
-        // Final check after loop ends
-        max = Math.max(max, sum);
-
+        max=Math.max(max,sum);
         return max;
     }
 }
+
