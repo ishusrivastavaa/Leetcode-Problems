928f948a76ef233682e988593654a3948e9d31e5
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2025-09-03 00:34:45 +0530

    Time: 1 ms (97.81%), Space: 43.9 MB (5.66%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.java
+++ 3463-alternating-groups-i/3463-alternating-groups-i.java
@@ -1,21 +1,37 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors) {
-        int nums[]=new int [colors.length+2];
-        nums[0]=colors[colors.length-1];
-        int count=0;
-        nums[nums.length-1]=colors[0];
-        int j=0;
-        for(int i=1 ; i<nums.length-1 ; i++){
-            nums[i]=colors[j];
-            j++;
+    //     int nums[]=new int [colors.length+2];
+    //     nums[0]=colors[colors.length-1];
+    //     int count=0;
+    //     nums[nums.length-1]=colors[0];
+    //     int j=0;
+    //     for(int i=1 ; i<nums.length-1 ; i++){
+    //         nums[i]=colors[j];
+    //         j++;
 
+    //     }
+    //     for(int i=1 ; i<nums.length-1 ; i++){
+    //         if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+    int count=0;
+    for(int i=1 ; i<colors.length-1 ; i++){
+        if(colors[i]!=colors[i-1] && colors[i]!=colors[i+1]){
+            count++;
         }
-        for(int i=1 ; i<nums.length-1 ; i++){
-            if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
-                count++;
-            }
-        }
-        return count;
     }
+    if(colors[0]!=colors[1] && colors[0]!=colors[colors.length-1]){
+        count++;
+    }
+    if(colors[colors.length-1]!=colors[colors.length-2]&& colors[colors.length-1]!=colors[0]){
+        count++;
+
+    }
+    return count;
+
     
+}
 }
