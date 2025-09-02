69f4ae7cc657d21a2bea04a896336835bbd01856
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2025-09-03 00:39:37 +0530

    Time: 1 ms (97.81%), Space: 43.1 MB (56.2%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.java
+++ 3463-alternating-groups-i/3463-alternating-groups-i.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int numberOfAlternatingGroups(int[] colors) {
+//class Solution {
+  //  public int numberOfAlternatingGroups(int[] colors) {
     //     int nums[]=new int [colors.length+2];
     //     nums[0]=colors[colors.length-1];
     //     int count=0;
@@ -17,21 +17,17 @@ class Solution {
     //     }
     //     return count;
     // }
-    int count=0;
-    for(int i=1 ; i<colors.length-1 ; i++){
-        if(colors[i]!=colors[i-1] && colors[i]!=colors[i+1]){
-            count++;
+   class Solution {
+    public int numberOfAlternatingGroups(int[] colors) {
+        int count = 0;
+        int n = colors.length;
+
+        for(int i=1;i<n - 1;i++){
+            if(colors[i-1] == colors[i+1] && colors[i] != colors[i-1] && colors[i] != colors[i+1]) count++;
         }
-    }
-    if(colors[0]!=colors[1] && colors[0]!=colors[colors.length-1]){
-        count++;
-    }
-    if(colors[colors.length-1]!=colors[colors.length-2]&& colors[colors.length-1]!=colors[0]){
-        count++;
+        if(colors[1] == colors[n-1] && colors[0] != colors[1] && colors[0] != colors[n-1]) count++;
+        if(colors[0] == colors[n-2] && colors[n-1] != colors[0] && colors[n-1] != colors[n-2]) count++;
 
-    }
     return count;
-
-    
-}
+    }
 }
