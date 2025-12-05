c74b35fd18d5b08da7836ce841ee553fe2134ee8
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2025-12-05 16:58:18 +0530

    Time: 1 ms (48.14%), Space: 43.5 MB (31.42%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int count=0;
+        int j=0;
+        
+        while(j+1 != nums.length){
+            
+            int val1=0;
+            int val2=0;
+            for(int i=j+1 ; i<nums.length ; i++){
+                val2+=nums[i];
+            }
+            for(int i=0 ; i<=j ;i++){
+                val1+=nums[i];
+            }
+            if((val1-val2) % 2==0){
+                count++;
+            }
+            j++;
+            
+            
+        }
+        return count;
+        
+    }
+}
+
+
