62289775313b23ef63a782a6f86448f9672702e4
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2026-01-21 00:21:20 +0530

    Time: 30 ms (92.14%), Space: 48.6 MB (38.14%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,10 +1,10 @@
 class Solution {
     public int distributeCandies(int[] arr) {
-        int tar = arr.length/2;
+        
         Set <Integer> set = new HashSet<>();
         for(int i=0 ;  i<arr.length ; i++){
             set.add(arr[i]);
         }
-        return Math.min(tar,set.size());
+        return Math.min(arr.length/2,set.size());
     }
 }
