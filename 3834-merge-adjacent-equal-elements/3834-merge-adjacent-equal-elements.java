9d02f5a110c6ad5b396e3ac2d280e70580d66a25
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2026-02-12 14:22:47 +0530

    Time: 106 ms (18.32%), Space: 128.2 MB (70.22%) - LeetHub

--- 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.java
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.java
@@ -1,18 +1,31 @@
 class Solution {
     public List<Long> mergeAdjacent(int[] nums) {
-        ArrayList <Long> list = new ArrayList<>();
-        for(int i=0 ; i<nums.length; i++){
-            list.add((long)nums[i]);
-        }
-        for(int i=0 ; i<list.size()-1 ; i++){
-            
-            if(i>=0 && list.get(i).equals(list.get(i+1))){
-                long sum = list.get(i)+list.get(i+1);
-                list.set(i , sum);
-                list.remove(i+1);
-                i=i-2;
+        ArrayList<Long> list = new ArrayList<>();
+        Stack<Long> stack = new Stack();
+        for(int i =0 ; i<nums.length ; i++){
+            if(!stack.isEmpty()&&stack.peek() == nums[i]){
+                long num = nums[i];
+                while(!stack.isEmpty() && num==stack.peek()){
+                    num = stack.pop()+num;
+                    
+                }
+                stack.push(num);
+                 
             }
-         }
-        return list;
+            else{
+                stack.push((long)nums[i]);
+            }
+            
+        }
+        ArrayList<Long> list1 = new ArrayList<>();
+        while(!stack.isEmpty()){
+            list.add(stack.pop());
+        }
+       for(int i=list.size()-1 ;i>=0 ; i--){
+        list1.add(list.get(i));
+        
+       }
+
+        return list1;
     }
 }
