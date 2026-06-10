c7099104caf78823f826d8af5fbfc59e16e18604
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2026-06-11 00:30:28 +0530

    Time: 2 ms (99.24%), Space: 72.2 MB (91.99%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int count5=0;
+        int count10=0;
+        int count20=0;
+        int i=0;
+        while(i<bills.length){
+            if(bills[i]==5){
+                count5++;
+            }
+            else if(bills[i]==10){
+                if(count5>=1){
+                    count5--;
+                    count10++;
+                }
+                else{
+                    return false;
+                }
+                
+            }
+            else {
+                if(count10>=1 && count5>=1){
+                    count10--;
+                    count5--;
+                }
+                else if(count5>=3){
+                    count5=count5-3;
+                }
+                else{
+                    return false;
+                }
+            }
+            i++;
+        }
+        return true;
+    }
+}
