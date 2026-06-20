38038a60d97461496dabd394ddc5f99205360fc9
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2026-06-20 17:36:15 +0530

    Time: 1 ms (38.5%), Space: 44.2 MB (33.96%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.java
+++ 2079-watering-plants/2079-watering-plants.java
@@ -1,15 +1,32 @@
 class Solution {
     public int wateringPlants(int[] plants, int capacity) {
-        int steps=0;
-        int cap=capacity;
-        for(int i=0 ; i<plants.length ; i++){
-            if(capacity<plants[i]){
-                steps=steps+i+i;
-                capacity=cap;
+        if(plants.length == 0) return 0;
+        int steps = 0;
+        int cap = capacity;
+        for(int i = 0 ; i < plants.length; i++){
+            if(plants[i] <= capacity){
+                capacity = capacity - plants[i];
+                steps =steps + 1;
+            }else if(capacity < plants[i]){
+                steps = steps + (i)*2+1 ;
+                capacity = cap;
+                capacity = capacity -plants[i];
             }
-            capacity=capacity-plants[i];
-            steps++;
         }
         return steps;
+        // for(int i=0;i<plants.length;i++){
+        //     if(plants[i]>capacity){
+        //         steps+=i;
+        //         steps+=i+1;
+        //         capacity=cap;
+        //     }
+        //         steps++;
+        //     capacity-=plants[i];
+        //     if(i!=plants.length-1 && capacity==0){
+        //         steps+=i+1;
+        //         capacity=cap;
+        //     }
+        // }
+        // return steps;
     }
 }
