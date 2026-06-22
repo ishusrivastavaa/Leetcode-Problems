5b195f634a5e128364950870614d3efdd969f7c3
Ishu Srivastava <168293450+ishusrivastavaa@users.noreply.github.com>
2026-06-22 23:43:54 +0530

    Time: 3 ms (81.87%), Space: 43.4 MB (65.22%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,30 +1,30 @@
 class Solution {
     public int calPoints(String[] ops) {
-        int result =0 ;
-        ArrayList<Integer> list = new ArrayList<>();
+        int result=0;
+        Stack<Integer>stack = new Stack<>();
         for(int i=0 ; i<ops.length ; i++){
             if(ops[i].equals("C")){
-                list.remove(list.size()-1);
+                stack.pop();
             }
             else if(ops[i].equals("D")){
-                int num = list.get(list.size()-1);
-                num = num*2;
-                list.add(num);
+                int num = stack.peek();
+                stack.push(num*2);
             }
             else if(ops[i].equals("+")){
-                int num = list.get(list.size()-1);
-                int num1 = list.get(list.size()-2);
-                list.add(num+num1);
+                int num = stack.pop();
+                int num1 = stack.peek();
+                stack.push(num);
+                stack.push(num+num1);
             }
             else{
-                int num = Integer.parseInt(ops[i]);
-                list.add(num);
+                int num= Integer.parseInt(ops[i]);
+                stack.push(num);
             }
+
         }
-        for(int i=0; i<list.size() ; i++){
-            result+=list.get(i);
+        while(!stack.isEmpty()){
+            result+=stack.pop();
         }
         return result;
-
     }
 }
